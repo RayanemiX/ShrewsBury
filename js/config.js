6b9850e4ca0db815f7b0f88ea009e707d4c071c6
Rayane <arayane08@gmail.com>
2026-09-09 10:34:04 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
-// =========================================================================
-// À REMPLIR : allez dans votre projet Supabase > Project Settings > API
-// =========================================================================
+// js/config.js
+
 const SUPABASE_URL = "https://xgjoeunbnjzrqhtlrmxx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhnam9ldW5ibmp6cnFodGxybXh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5NDAzMDYsImV4cCI6MjEwNDUxNjMwNn0.a0OmAwbSSJoWLkO7eK5MFTRnPt4zxS2jgexlEQcWkQA";
